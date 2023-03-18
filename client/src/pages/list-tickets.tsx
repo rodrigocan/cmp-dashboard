4d428c91d0e8a3db658549e31f877ac0afe18e5c
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-03-18 17:16:56 -0300

    formatação da data na listagem dos chamados

--- client/src/pages/list-tickets.tsx
+++ client/src/pages/list-tickets.tsx
@@ -6,11 +6,25 @@ import {
 } from "@pankod/refine-mui"
 
 const columns: GridColumns = [
-  { field: "createdAt", headerName: "Aberto em", width: 200 },
+  { field: "createdAt", headerName: "Aberto em", width: 150, valueFormatter: (params) => {
+    if (params.value == null) {
+      return ""
+    }
+
+    const valueFormatted = new Date(params.value).toLocaleString("pt-BR", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit"
+    })
+
+    return valueFormatted
+  }},
   { field: "property", headerName: "Imóvel", width: 200 },
   { field: "sector", headerName: "Setor", width: 200 },
-  { field: "service", headerName: "Serviço", width: 200 },
-  { field: "status", headerName: "Status", width: 200 }
+  { field: "service", headerName: "Serviço", width: 250 },
+  { field: "status", headerName: "Status", width: 150 }
 ]
 
 const ListTickets = () => {
